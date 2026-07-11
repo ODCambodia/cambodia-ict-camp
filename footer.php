a04e0f38e8b3187164f2163e55c9091916e856d1
S-mardii <s.mardy009@gmail.com>
2026-07-11 16:18:57 +0700

    [IMP] Add offset to handle AOS

--- footer.php
+++ footer.php
@@ -47,6 +47,7 @@ do_action( 'event_star_action_after' );
                 AOS.init({
                     duration: 800,          // Values from 0 to 3000, with step 50ms
                     easing: 'ease-in-out',  // Default easing for AOS animations
+                    offset: -200
                     once: true              // Whether animation should happen only once - while scrolling down
                 });
             });
